2fc5aff84b89b0123e801092d5108e76b0f0d073
ChaOSChriS <chaosware19@gmail.com>
2014-08-06 11:55:46 +0200

    buildscript: fix syntax

--- buildme.sh
+++ buildme.sh
@@ -119,7 +119,7 @@ cd $SD/out/$CODENAME/
  #create zip and clean folder
     echo "[BUILD]: Creating zip: MiRaGe_any_"$CODENAME"_"$VERSION"_"$DATE".zip ...";
     zip -r MiRaGe_any_"$CODENAME"_"$VERSION"_"$DATE"_"$DATEE".zip . -x "*.zip" "*.sha1" "*.md5"
-echo "[BUILD]: Creating changelog: MiRaGe_any_"$CODENAME"_$VERSION"_"$DATE".txt ...";
+echo "[BUILD]: Creating changelog: MiRaGe_any_"$CODENAME"_"$VERSION"_"$DATE".txt ...";
 cd $SD
 git log --pretty=format:'%h (%an) : %s' --graph $REV^..HEAD > $SD/out/$CODENAME/MiRaGe_any_"$CODENAME"_"$VERSION"_"$DATE"_"$DATEE".txt
     echo "[BUILD]: Done!...";
